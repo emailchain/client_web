046e7f7e533180f8f3ddb22a923b4c625ff3bc9f
eyuelberga <eyuel.berga@yahoo.com>
2020-01-28 18:05:38 +0300

    validation on encryption

--- src/views/new_message.vue
+++ src/views/new_message.vue
@@ -1,31 +1,41 @@
 <template>
-  <v-card
-    max-width="800"
-    class="mx-auto mt-9"
-  >
-<v-card-text>
-        <v-text-field v-model="to" label="To:" ></v-text-field>
 
-</v-card-text>
-    <v-card-text>
-            <v-text-field  outlined v-model="subject" label="Subject" ></v-text-field>
+    <v-card
+            max-width="800"
+            class="mx-auto mt-9"
+    >
+        <v-snackbar
+                color="error"
+                top
+                v-model="snackbar"
+                :timeout=2000
+        >
+            {{ errorText }}
+        </v-snackbar>
+        <v-card-text>
+            <v-text-field v-model="to" label="To:"></v-text-field>
 
-    <v-textarea v-model="message"
-      outlined
-      label="message"
-      auto-grow
-    ></v-textarea></v-card-text>
-    <v-card-actions>
-      <v-btn
-        class="primary"
-        color="deep-purple accent-4"
-        @click="sendMail"
-        
-      >
-        Send
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        </v-card-text>
+        <v-card-text>
+            <v-text-field outlined v-model="subject" label="Subject"></v-text-field>
+
+            <v-textarea v-model="message"
+                        outlined
+                        label="message"
+                        auto-grow
+            ></v-textarea>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+                    class="primary"
+                    color="deep-purple accent-4"
+                    @click="sendMail"
+
+            >
+                Send
+            </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 <script>
     import EmailCrypto from "../crypto";
@@ -36,26 +46,33 @@
                 to: "",
                 subject: "",
                 message: "",
+                errorText: "",
+                snackbar: false
             };
         },
         methods: {
-            sendMail() {
-                const key =  EmailCrypto.generateKey(localStorage.getItem("RSAKey"));
+            async sendMail() {
+                const key = EmailCrypto.generateKey(localStorage.getItem("RSAKey"));
                 const mail = {
                     subject: this.subject,
                     message: this.message
                 };
-                const msg =JSON.stringify(mail)
-               const encryption = EmailCrypto.encrypt(msg,this.to,key);
-               const payload = {
-                   sender:EmailCrypto.pubKey(key),
-                   recipient:this.to,
-                   message:msg
-               }
-                // eslint-disable-next-line no-console
-               console.log(encryption)
-                // eslint-disable-next-line no-console
-                console.log(payload)
+                const msg = JSON.stringify(mail)
+                const encryption = EmailCrypto.encrypt(msg, this.to, key);
+                if (encryption.status === "success") {
+                    const payload = {
+                        sender: EmailCrypto.pubKey(key),
+                        recipient: this.to,
+                        message: msg
+                    };
+                    // eslint-disable-next-line no-console
+                    console.log(encryption);
+                    // eslint-disable-next-line no-console
+                    console.log(payload)
+                } else {
+                    this.errorText = encryption.status;
+                    this.snackbar = true;
+                }
             }
         },
     }
