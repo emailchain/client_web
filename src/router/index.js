fc4dada212781ed1321a4bfee2ed290e460d9742
mulugeta <mulugetaewnu1@.com>
2020-01-28 16:31:49 +0300

    creat new page its peer

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/new_message.vue')
+  },
+  {
+    path: '/peer',
+    name: 'peer',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/connect_peer.vue')
   }
 ]
 
---
+++ src/views/connect_peer.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="8">
+                <v-text-field outlined
+                              v-model="passPhrase"
+                              label="peer"
+                              required
+                ></v-text-field>
+            </v-col>
+            <v-col cols="4">
+                <v-btn  @click="generateKey" large>connect</v-btn>
+            </v-col>
+        </v-row>
+
+        <v-card 
+                class="mx-auto">
+            <v-card-title>Connected Peers</v-card-title>
+        <v-list>
+      
+      <v-list-item-group v-model="item" color="primary">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+        </v-card>
+    </v-container>
+</template>
+<script>
+  export default {
+    data: () => ({
+      item: 1,
+      items: [
+        
+        { text: 'http:/gsggs', icon: 'mdi-account' },
+      ],
+    }),
+  }
+</script>
